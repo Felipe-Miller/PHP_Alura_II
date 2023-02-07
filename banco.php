d538384aa7dbd04d428dfe185fb345cc20fd0d94
Felipe-Miller <fmvargas@protonmail.com>
2023-02-07 14:24:32 -0300

    .

--- banco.php
+++ banco.php
@@ -1,28 +1,33 @@
 <?php
 
 $contasCorrentes = [
-   
-    '123.456.789-10'  => [
-        'titular' => 'Titular: Maria',
-        'saldo' => 'Saldo: 1000',
+    '123.456.789-10' => [
+        'titular' => 'Maria',
+        'saldo' => 10000
     ],
-
     '123.456.789-11' => [
-        'titular' => 'Titular: Maria',
-        'saldo' => 'Saldo: 1000',
-    ], 
-    
-    '123.456.789-12' => [
-        'titular' => 'Titular: Alberto',
-        'saldo' => 'Saldo: 300'
+        'titular' => 'Alberto',
+        'saldo' => 300
+    ],
+    '123.256.789-12' => [
+        'titular' => 'Vinicius',
+        'saldo' => 100
     ]
-
 ];
 
 $contasCorrentes['123.456.789-10']['saldo'] -= 500;
+$contasCorrentes['123.456.789-11']['saldo'] -= 500;
+
+if (500 > $contasCorrentes['123.456.789-11']['saldo']) {
+    
+    echo "Você não pode sacar este valor" . PHP_EOL;
+
+} else 
+
+    $contasCorrentes['123.456.789-11']['saldo'] -= 500;
 
 foreach ($contasCorrentes as $cpf => $conta) {
-  
-    echo $cpf . " " . $conta['titular'] . PHP_EOL;
 
+    echo $cpf . " " . $conta['titular'] . ' ' . $conta['saldo'] . PHP_EOL;
+    
 }
