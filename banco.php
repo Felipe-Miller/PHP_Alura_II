8a5737aa69794e669e7610c6a7052e1f68bbcdc2
Felipe-Miller <email@youremail.com>
2023-02-08 22:02:14 -0300

    .

--- banco.php
+++ banco.php
@@ -1,18 +1,22 @@
 <?php
 
 $contasCorrentes = [
+ 
     '123.456.789-10' => [
         'titular' => 'Maria',
         'saldo' => 10000
     ],
+
     '123.456.689-11' => [
         'titular' => 'Alberto',
         'saldo' => 300
     ],
+
     '123.256.789-12' => [
         'titular' => 'Vinicius',
         'saldo' => 100
     ]
+    
 ];
 
 function sacar(array $conta, float $valorASacar): array
@@ -77,7 +81,7 @@ $contasCorrentes['123.256.789-12'] = depositar(
 foreach ($contasCorrentes as $cpf => $conta) {
     
     exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
-    
+
 }
 
 
