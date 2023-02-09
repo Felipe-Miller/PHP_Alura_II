fc2c262cdb319b91d61f91359a8404e7f8178038
Felipe-Miller <email@youremail.com>
2023-02-08 22:00:46 -0300

    .

--- banco.php
+++ banco.php
@@ -1,18 +1,11 @@
 <?php
 
-<<<<<<< HEAD
-=======
-exibeMensagem ($mensagem) {
-    echo $mensagem . PHP_EOL;
-}
-
->>>>>>> 746708944227b2637a0a13d8803b365ee2db06b7
 $contasCorrentes = [
     '123.456.789-10' => [
         'titular' => 'Maria',
         'saldo' => 10000
     ],
-    '123.456.789-11' => [
+    '123.456.689-11' => [
         'titular' => 'Alberto',
         'saldo' => 300
     ],
@@ -22,24 +15,71 @@ $contasCorrentes = [
     ]
 ];
 
-<<<<<<< HEAD
+function sacar(array $conta, float $valorASacar): array
+{
+    if ($valorASacar > $conta['saldo']) {
+        
+        exibeMensagem("Você não tem saldo suficiente");
 
-foreach ($contasCorrentes as $cpf => $conta) {
-    echo $cpf . " " . $conta['titular'] . PHP_EOL;
-=======
-$contasCorrentes['123.456.789-10']['saldo'] -= 500;
-$contasCorrentes['123.456.789-11']['saldo'] -= 500;
+    } else {
+        
+        $conta['saldo'] -= $valorASacar;
 
-if (500 > $contasCorrentes['123.456.789-11']['saldo']) {
-    
-    echo "Você não pode sacar este valor" . PHP_EOL;
+    }
 
-} else 
-
-    $contasCorrentes['123.456.789-11']['saldo'] -= 500;
-
-foreach ($contasCorrentes as $cpf => $conta) {
-    exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
-
->>>>>>> 746708944227b2637a0a13d8803b365ee2db06b7
+    return $conta;
 }
+
+function exibeMensagem(string $mensagem)
+
+{
+    echo $mensagem . PHP_EOL;
+}
+
+function depositar(array $conta, float $valorADepositar): array
+
+{
+    if ($valorADepositar > 0) {
+        
+        $conta['saldo'] += $valorADepositar;
+
+    } else {
+        
+        exibeMensagem("Depositos precisam ser positivos");
+
+    }
+   
+    return $conta;
+
+}
+
+$contasCorrentes['123.456.789-10'] = sacar(
+    
+    $contasCorrentes['123.456.789-10'],
+    500
+
+);
+
+$contasCorrentes['123.456.689-11'] = sacar(
+    
+    $contasCorrentes['123.456.689-11'],
+    200
+
+);
+
+$contasCorrentes['123.256.789-12'] = depositar(
+    
+    $contasCorrentes['123.256.789-12'],
+    900
+
+);
+
+foreach ($contasCorrentes as $cpf => $conta) {
+    
+    exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
+    
+}
+
+
+
+
