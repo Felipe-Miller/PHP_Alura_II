9f4cfddaba65236e7c4930809a510b01aa8be5e9
Felipe-Miller <email@youremail.com>
2023-02-10 19:16:32 -0300

    .

--- banco.php
+++ banco.php
@@ -1,5 +1,7 @@
 <?php
 
+include('funcoes.php');
+
 $contasCorrentes = [
 
     '123.456.789-10' => [
@@ -18,44 +20,6 @@ $contasCorrentes = [
     ]
 
 ];
-
-function sacar(array $conta, float $valorASacar): array
-{
-    if ($valorASacar > $conta['saldo']) {
-
-        exibeMensagem("Você não tem saldo suficiente");
-
-    } else {
-
-        $conta['saldo'] -= $valorASacar;
-
-    }
-
-    return $conta;
-}
-
-function exibeMensagem(string $mensagem)
-
-{
-    echo $mensagem . PHP_EOL;
-}
-
-function depositar(array $conta, float $valorADepositar): array
-
-{
-    if ($valorADepositar > 0) {
-
-        $conta['saldo'] += $valorADepositar;
-
-    } else {
-
-        exibeMensagem("Depositos precisam ser positivos");
-
-    }
-
-    return $conta;
-
-}
 
 $contasCorrentes['123.456.789-10'] = sacar(
 
